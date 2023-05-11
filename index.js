7b2ae9bd3622e89364a612af21ad96a540496493
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-05-11 23:31:46 +0530

    added another post route

--- index.js
+++ index.js
@@ -16,7 +16,30 @@ app.post('/', (req, res) => {
     try {
         const { email, password } = req.body
 
-        if (!email || !password ) {
+        if (!email || !password) {
+            return res.status(401).json({
+                type: "error",
+                message: "Invalid or No Credentials",
+            });
+        }
+
+        res.status(200).json({
+            type: "success",
+            message: "Logged in successfully",
+        })
+    } catch (error) {
+        res.json({
+            type: "error",
+            message: error.message,
+        })
+    }
+})
+
+app.post('/signup', (req, res) => {
+    try {
+        const { name, email, password, age, weight } = req.body
+
+        if (!name || !email || !password || !age || !weight) {
             return res.status(401).json({
                 type: "error",
                 message: "Invalid or No Credentials",
